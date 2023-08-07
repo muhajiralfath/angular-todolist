fb894e2f4b65a915df94c5770cef069ef61865df
Tika Yesi Kristiani <yesikristianitika@gmail.com>
2023-08-07 11:39:57 +0700

    learning component in angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { TikaComponent } from './tika/tika/tika.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    TikaComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'hello-world',
+    templateUrl: './hello.component.html',
+    styleUrls: ['./hello.component.css']
+})
+
+export class HelloComponent{}
